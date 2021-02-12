4f151fdae729596db55bb078a0b96a943978562c
Brandon Taylor <taylorbm@gmail.com>
2021-02-12 09:08:28 -0500

    added wsl2-network

---
+++ wsl2-network.ps1
@@ -0,0 +1,64 @@
+﻿# WSL2 network port forwarding script v1
+#   for enable script, 'Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser' in Powershell,
+#   for delete exist rules and ports use 'delete' as parameter, for show ports use 'list' as parameter.
+#   written by Daehyuk Ahn, Aug-1-2020
+#
+#   Tweaks made by Brandon Taylor
+
+# Display all portproxy information
+If ($Args[0] -eq "list") {
+    netsh interface portproxy show v4tov4;
+    exit;
+} 
+
+# If elevation needed, start new process
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path),"$Args runas" -Verb RunAs
+  exit
+}
+
+# You should modify '$Ports' for your applications 
+$Ports = (4444,8000,8080,9999)
+    
+# Check WSL ip address
+wsl hostname -I | Set-Variable -Name "WSL"
+$found = $WSL -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+if (-not $found) {
+  echo "WSL2 cannot be found. Terminate script.";
+  exit;
+}
+
+# Remove and Create NetFireWallRule
+Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock';
+if ($Args[0] -ne "delete") {
+  New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $Ports -Action Allow -Protocol TCP;
+  New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $Ports -Action Allow -Protocol TCP;
+}
+
+#delete all previous v4tov4 rules
+$Addr = "0.0.0.0"
+$regex = [regex] '(\d{2,5}$)'
+$prevRoutePorts = iex "netsh interface portproxy show v4tov4" | Select-String '(\d{2,5}$)' -AllMatches | Foreach {$_.Matches} | Foreach{$_.Value};
+Foreach($port in $prevRoutePorts) {
+  iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$Addr";
+  echo "deleted previously v4tov4 rule on port $port";
+}
+
+# Add each port into portproxy
+Foreach ($Port in $Ports) {
+    iex "netsh interface portproxy delete v4tov4 listenaddress=$Addr listenport=$Port | Out-Null";
+    if ($Args[0] -ne "delete") {
+        iex "netsh interface portproxy add v4tov4 listenaddress=$Addr listenport=$Port connectaddress=$WSL connectport=$Port | Out-Null";
+    }
+}
+
+# Display all portproxy information
+netsh interface portproxy show v4tov4;
+
+# Give user to chance to see above list when relaunched start
+If ($Args[0] -eq "runas" -Or $Args[1] -eq "runas") {
+  Write-Host -NoNewLine 'Press any key to close! ';
+  $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
